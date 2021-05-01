90e9e478533a0e01e7a1eedd9e7c7632fd94690b
SharkEzz <icraft640@gmail.com>
2021-05-01 12:22:18 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TopNavbar from './Components/UI/TopNavbar';
+
+function App() {
+  return (
+    <TopNavbar />
+  );
+}
+
+export default App;
---
+++ src/Components/UI/TopNavbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+const TopNavbar = () => (
+  <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar.Brand href="#">SatTrack</Navbar.Brand>
+  </Navbar>
+);
+
+export default TopNavbar;
